4efbd0a9be17277b187489b617bd122a9697aa8f
Emahrb <36061510+Emahrb@users.noreply.github.com>
2019-12-31 21:56:26 +0600

    added BS and RS and plugins and created Navbar

---
+++ src/pages/team.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TeamPage = () => (
+  <Layout>
+    <SEO title="Team" keyword={["gatsby", "application", "react"]} />
+    <h1>Team...</h1>
+  </Layout>
+)
+
+export default TeamPage
